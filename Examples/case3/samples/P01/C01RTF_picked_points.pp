8354bae7237128293988ac648b4081834c9f1d7b
aaronsfox <mfkeast@deakin.edu.au>
2022-03-04 17:03:10 +1100

    landmark files for both mean model and sample 01

---
+++ Examples/case3/samples/P01/C01RTF_picked_points.pp
@@ -0,0 +1,20 @@
+<!DOCTYPE PickedPoints>
+<PickedPoints>
+ <DocumentData>
+  <DateTime date="2022-03-04" time="16:32:01"/>
+  <User name="Deakin Uni"/>
+  <DataFileName name="C01RTF.stl"/>
+  <templateName name=""/>
+ </DocumentData>
+ <point active="1" x="-162.0939" name="0" z="-470.85196" y="-58.494228"/>
+ <point active="1" x="-159.98387" name="1" z="-495.17874" y="-59.639042"/>
+ <point active="1" x="-156.81781" name="2" z="-517.72583" y="-57.86594"/>
+ <point active="1" x="-156.30327" name="3" z="-543.84186" y="-56.131016"/>
+ <point active="1" x="-154.07077" name="4" z="-569.76416" y="-53.997139"/>
+ <point active="1" x="-152.94026" name="5" z="-598.58905" y="-50.922577"/>
+ <point active="1" x="-153.29999" name="6" z="-626.66302" y="-47.567249"/>
+ <point active="1" x="-151.71202" name="7" z="-655.42871" y="-43.58971"/>
+ <point active="1" x="-150.07175" name="8" z="-684.65778" y="-39.352081"/>
+ <point active="1" x="-145.76402" name="9" z="-713.41064" y="-36.149361"/>
+ <point active="1" x="-140.50854" name="10" z="-749.95557" y="-37.310425"/>
+</PickedPoints>
---
+++ ShapeModels/tibia-fibula/tibiaFibulaShapeModel_mean_picked_points.pp
@@ -0,0 +1,35 @@
+<!DOCTYPE PickedPoints>
+<PickedPoints>
+ <DocumentData>
+  <DateTime date="2022-03-04" time="16:52:46"/>
+  <User name="Deakin Uni"/>
+  <DataFileName name="tibiaFibulaShapeModel_mean.stl"/>
+  <templateName name=""/>
+ </DocumentData>
+ <point name="LC" z="35.660679" active="1" y="383.6745" x="12.542912"/>
+ <point name="MC" z="-27.519869" active="1" y="378.36954" x="-27.328461"/>
+ <point name="LM" z="32.383232" active="1" y="-3.8868353" x="1.8839755"/>
+ <point name="MM" z="-30.097565" active="1" y="10.755511" x="-4.508285"/>
+ <point name="TT" z="-11.090926" active="1" y="357.07172" x="28.761148"/>
+ <point name="FH" z="48.235939" active="1" y="369.0744" x="-0.5612216"/>
+ <point name="T1" z="-6.018311" active="1" y="358.23462" x="30.345831"/>
+ <point name="T2" z="-8.2458439" active="1" y="337.36682" x="29.771832"/>
+ <point name="T3" z="-8.4586372" active="1" y="317.37131" x="26.663731"/>
+ <point name="T4" z="-8.7669754" active="1" y="290.88531" x="24.13361"/>
+ <point name="T5" z="-7.5931377" active="1" y="259.65067" x="21.829201"/>
+ <point name="T6" z="-9.054636" active="1" y="234.16096" x="21.254667"/>
+ <point name="T7" z="-7.9695311" active="1" y="208.21605" x="20.796772"/>
+ <point name="T8" z="-5.6902041" active="1" y="179.51691" x="20.164639"/>
+ <point name="T9" z="-4.9420652" active="1" y="145.52373" x="17.554636"/>
+ <point name="T10" z="-3.5191512" active="1" y="108.23522" x="14.656423"/>
+ <point name="F1" z="42.940323" active="1" y="324.6713" x="0.353291"/>
+ <point name="F2" z="41.388729" active="1" y="295.85147" x="-2.3874514"/>
+ <point name="F3" z="39.379749" active="1" y="267.5744" x="-2.5985487"/>
+ <point name="F4" z="38.30711" active="1" y="242.11555" x="-3.8622148"/>
+ <point name="F5" z="36.720154" active="1" y="215.12303" x="-4.607904"/>
+ <point name="F6" z="35.070988" active="1" y="180.66516" x="-7.1890244"/>
+ <point name="F7" z="33.192444" active="1" y="157.1801" x="-6.5375962"/>
+ <point name="F8" z="30.232111" active="1" y="125.25513" x="-3.056864"/>
+ <point name="F9" z="28.980736" active="1" y="97.450066" x="-2.1600311"/>
+ <point name="F10" z="27.90646" active="1" y="65.969299" x="-2.1613014"/>
+</PickedPoints>
